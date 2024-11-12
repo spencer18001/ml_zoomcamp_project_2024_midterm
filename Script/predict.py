23bb13486691d318ebdeca15c664d2c7879fbfdc
spencer0787 <spencer0787@cchntek.com>
2024-11-12 01:09:44 -0400

    commit

---
+++ Script/predict.py
@@ -0,0 +1,40 @@
+import pickle
+import xgboost as xgb
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+target_name = "Win rate"
+model_file = "model.bin"
+
+with open(model_file, "rb") as f_in:
+    model_name, dv, model = pickle.load(f_in)
+
+features = list(dv.get_feature_names_out())
+
+app = Flask("lol")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    player = request.get_json()
+
+    X = dv.transform([player])
+
+    if model_name == "linear regression":
+        y_pred = model.predict(X)
+    elif model_name == "decision tree":
+        y_pred = model.predict(X)
+    elif model_name == "random forest":
+        y_pred = model.predict(X)
+    elif model_name == "xgboost":
+        dmatrix = xgb.DMatrix(X, feature_names=features)
+
+        y_pred = model.predict(dmatrix)
+
+    result = {
+        target_name: y_pred,
+    }
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
---
+++ Script/train.py
@@ -0,0 +1,93 @@
+import json, pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+import xgboost as xgb
+
+target_name = "Win rate"
+seed = 1
+model_file = "model.bin"
+
+### data preprocessing
+df_path = "Data/player_statistics_cleaned_final.csv"
+df = pd.read_csv(df_path)
+
+# exclude these features
+df.drop(columns=["TeamName", "PlayerName", "Country"], inplace=True)
+
+# "Solo Kills" should be a numerical feature
+# based on dataset documentation, "-" likely indicates 0
+df["Solo Kills"] = df["Solo Kills"].replace(to_replace="-", value=0)
+df["Solo Kills"] = df["Solo Kills"].astype(int)
+
+# remove "Penta Kills" as all values are 0
+df.drop(columns=["Penta Kills"], inplace=True)
+
+# using only df_full_train and df_test due to small dataset size and planned cross-validation
+df_full_train, df_test = train_test_split(df, test_size=0.2, random_state=seed)
+
+df_full_train = df_full_train.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+y_full_train = df_full_train[target_name].values
+y_test = df_test[target_name].values
+
+del df_full_train[target_name]
+del df_test[target_name]
+
+# encode features
+dv = DictVectorizer(sparse=False)
+
+full_train_dict = df_full_train.to_dict(orient="records")
+X_full_train = dv.fit_transform(full_train_dict)
+
+test_dict = df_test.to_dict(orient="records")
+X_test = dv.fit_transform(test_dict)
+
+features = list(dv.get_feature_names_out())
+
+
+### training
+with open("best_model_params.json", "r") as infile:
+    json_object = json.load(infile)
+
+best_model_name = json_object["model_name"]
+param_dict = json_object["params"]
+
+if best_model_name == "linear regression":
+    if param_dict["alpha"] == 0:
+        model = LinearRegression()
+    else:
+        model = Ridge(**param_dict, random_state=seed)
+    model.fit(X_full_train, y_full_train)
+    y_pred = model.predict(X_test)
+elif best_model_name == "decision tree":
+    model = DecisionTreeRegressor(**param_dict, random_state=seed)
+    model.fit(X_full_train, y_full_train)
+    y_pred = model.predict(X_test)
+elif best_model_name == "random forest":
+    model = RandomForestRegressor(**param_dict, random_state=seed, n_jobs=-1)
+    model.fit(X_full_train, y_full_train)
+    y_pred = model.predict(X_test)
+elif best_model_name == "xgboost":
+    dfulltrain = xgb.DMatrix(X_full_train, label=y_full_train,
+                    feature_names=features)
+    dtest = xgb.DMatrix(X_test, feature_names=features)
+
+    params = {
+        "seed": seed,
+        "objective": 'reg:squarederror',
+        "nthread": 8,
+        "verbosity": 1,
+    } | param_dict
+    model = xgb.train(params, dfulltrain, num_boost_round=200)
+    y_pred = model.predict(y_test)
+
+root_mean_squared_error(y_test, y_pred)
+
+with open(model_file, "wb") as f_out:
+    pickle.dump((param_dict, dv, model), f_out)
